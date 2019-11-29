f27a55292bbe269ec50a280400ba9610b86e72d7
lightionight <lightionight@aol.com>
2019-11-29 15:46:59 +0800

    完成2D及3D的基本自动寻路,了解unity的四大坐标系的转换,Nav Mesh很强大

--- Assets/Scripts/Level_05/BirdFlyV2.cs
+++ Assets/Scripts/Level_05/BirdFlyV2.cs
@@ -3,16 +3,17 @@
 public class BirdFlyV2 : MonoBehaviour
 {
     /*********静态变量************/
-    private static readonly float speed = 0.001f;
+    private static readonly float speed =10f;
     /*********变量************/
     private float t;
-    public Vector2 mousePoint;
-    RectTransform birdRectTransform;
-    RectTransform parentRectTransform;
+    private Vector2 mousePoint;
+    private RectTransform birdRectTransform;
+    private RectTransform parentRectTransform;
     // Use this for initialization
     void Start()
     {
         birdRectTransform = GetComponent<RectTransform>();
+        birdRectTransform.SetAsLastSibling();
         parentRectTransform = GameObject.Find("Canvas").GetComponent<RectTransform>();
         
 
@@ -22,10 +23,18 @@ public class BirdFlyV2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //movePoint = mousePoint;//vec2转换为vec3
-        t = (1 / ((birdRectTransform.anchoredPosition - mousePoint).magnitude)) * speed;       
-        mousePoint = CurrentMousePosition(parentRectTransform);
-        birdRectTransform.anchoredPosition = Vector2.Lerp(birdRectTransform.anchoredPosition, mousePoint, t);
+        
+        if(Input.GetMouseButtonDown(0))
+        {
+            mousePoint = CurrentMousePosition(parentRectTransform);
+            t = (1 / ((birdRectTransform.anchoredPosition - mousePoint).magnitude)) * speed;
+
+
+        }
+        birdRectTransform.anchoredPosition = Vector2.LerpUnclamped(birdRectTransform.anchoredPosition, mousePoint, t);
+        Debug.Log(t.ToString());
+        //Debug.Log($"This is anchored{ birdRectTransform.anchoredPosition.ToString()}");
+        //Debug.Log($"This is mousePoint{mousePoint.ToString()}");
         
         
         
@@ -36,5 +45,9 @@ public class BirdFlyV2 : MonoBehaviour
         RectTransform parentRectTransform = rectTransform;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, Input.mousePosition, GameObject.FindWithTag("MainCamera").GetComponent<Camera>(), out currentMousePosition);
         return currentMousePosition;
+    }
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(0, 20, 150, 80), "Anchor Position X : ");
     }
 }
---
+++ Assets/Scripts/Level_06/MouseToPosition.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MouseToPosition : MonoBehaviour
+{
+    private Transform playerTransform;
+    private Vector3 mouseClickPosition;
+    private float t;
+    private static readonly float speed = 5f;
+    private NavMeshAgent playerAgent;
+    GameObject clickEffect;
+    private void Start()
+    {
+        playerTransform = transform;
+        playerAgent = gameObject.GetComponent<NavMeshAgent>();
+        clickEffect = GameObject.Find("ClickEffect");
+        clickEffect.GetComponent<ParticleSystem>().Pause(true);
+    }
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GetMouseClickWorldPostion();
+            MouseClickEffect();
+            //t = 1 / ((playerTransform.position - mouseClickPosition).magnitude) * speed;
+            playerAgent.speed = 5f;
+            playerAgent.height = 5f;
+            playerAgent.SetDestination(mouseClickPosition);
+
+        }
+    }
+    private void GetMouseClickWorldPostion()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            mouseClickPosition = hit.point;
+
+        }
+    }
+    private void MouseClickEffect()
+    {
+        clickEffect.transform.position = mouseClickPosition;
+        clickEffect.GetComponent<ParticleSystem>().Play(true);
+    }
+}
