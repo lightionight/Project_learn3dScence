e34e0ab925cd527152059bfc2d9250d093c2b364
lightionight <lightionight@aol.com>
2019-12-02 17:40:23 +0800

    使用2d物理引擎,建立2d操控通用类

---
+++ Assets/Scripts/Level_05/CharacterMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMove : MonoBehaviour
+{
+    CharacterControllerBase characterControllerBase;
+    void Start()
+    {
+        characterControllerBase = new CharacterControllerBase();   
+    }
+    void Update()
+    {
+        characterControllerBase._base2dController(gameObject);
+
+    }
+}
---
+++ Assets/Scripts/Level_05/Enemy_SixStar.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_SixStar : MonoBehaviour
+{
+    private float yMin;
+    private float yMax;
+    private float speed = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        yMin = transform.position.y - 5f;
+        yMax = transform.position.y + 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AutoMove(gameObject);
+    }
+    void AutoMove(GameObject enemy)
+    {
+        enemy.transform.position += Vector3.up * Time.deltaTime * speed;
+        if (enemy.transform.position.y >= yMax)
+        {
+            
+            speed = -speed;
+        }
+        else if(enemy.transform.position.y <= yMin)
+        {
+            speed = -speed;
+        }
+        
+
+    }
+}
