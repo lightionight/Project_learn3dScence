5e0f5366587a19b86d902199e25813c91d587f02
lightionight <lightionight@aol.com>
2019-12-04 00:17:19 +0800

    解决一部分2d人物基础动作问题

--- Assets/Scripts/Base/CharacterControllerBase.cs
+++ Assets/Scripts/Base/CharacterControllerBase.cs
@@ -6,17 +6,24 @@ public class CharacterControllerBase : MonoBehaviour
     
 
     //3D Parameter
-
     //2D基本控制
     public void _base2dController(GameObject player, bool isJump)
     {
-        Vector2 moveSpeed = new Vector2(3f, 10f);
-        float jump = Input.GetAxisRaw("Jump");
-        float leftRight = Input.GetAxisRaw("Horizontal");
-        Rigidbody2D playerRigi2d = player.GetComponent<Rigidbody2D>();
+        Animator animatorPlayer = player.GetComponent<Animator>();//获取2d玩家角色的动画控件
+        Rigidbody2D playerRigi2d = player.GetComponent<Rigidbody2D>();//获取2d玩家的Rigi控件
+        Vector2 moveSpeed = new Vector2(3f, 10f);//设置跳跃及移动放大倍数
+        float jump = Input.GetAxisRaw("Jump");//获取用户输入跳跃数值
+        float leftRight = Input.GetAxisRaw("Horizontal");//获取用户输入
+
+
         if (isJump)
         {
+            animatorPlayer.Play("Jump", 0);
             playerRigi2d.velocity = new Vector2(leftRight, jump) * moveSpeed;
+        }
+        else
+        {
+            animatorPlayer.Play("StandBy", 0);
         }
         
 
--- Assets/Scripts/Level_05/MarioContorller.cs
+++ Assets/Scripts/Level_05/MarioContorller.cs
@@ -4,16 +4,38 @@ using System.Collections;
 public class MarioContorller : MonoBehaviour
 {
     CharacterControllerBase characterControllerBase;
+    private bool animatorIsJump;
+    private bool isJump = false;
 
     // Use this for initialization
     void Start()
     {
         characterControllerBase = gameObject.AddComponent<CharacterControllerBase>();
+        animatorIsJump = gameObject.GetComponent<Animator>().GetBool("isJump"); //获取animator里面的isJump参数；
     }
-
     // Update is called once per frame
     void Update()
     {
-
+        if(isJump)
+        {
+            characterControllerBase._base2dController(gameObject, isJump);
+            if(gameObject.GetComponent<Rigidbody2D>().velocity.y != 0)
+            {
+                isJump = false;
+            }
+            else
+            {
+                isJump = true;
+            }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        Debug.Log(collision2D.gameObject.ToString());
+        if (collision2D.gameObject.tag == "Ground")
+        {
+            Debug.Log("Touch Ground");
+            isJump = true;
+        }
     }
 }
